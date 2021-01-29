0b2d9717307b2fd7f5cee3dd757fd3925fa8f6ea
Connor Finley <connor.finley5@gmail.com>
2021-01-28 21:29:04 -0500

    Add ability to search for pages in navbar

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -10,31 +10,48 @@ const Navbar = props => {
   const dispatch = useDispatch()
   const searchQuery = useSelector(state => state.navbar.searchQuery)
   const blocks = useSelector(state => state.blocks)
-  const searchResultBlocks = Object.values(blocks)
-    .filter(block => {
-      return block.text.indexOf(searchQuery) !== -1
-    })
 
-  const suggestions = searchResultBlocks.map(block => (
-    <PageLink pageBlockId={block.id} className="list-group-item list-group-item-action bg-dark text-light" key={block.id}>{block.text}</PageLink>
-  ))
+  const clearSearch = () => {
+    dispatch(updateSearchQuery({ query: '' }))
+  }
+
+  let suggestions = []
+  if (searchQuery && searchQuery.length) {
+    const searchResultBlocks = Object.values(blocks)
+      .filter(block => {
+        return !block.parentId && block.text.indexOf(searchQuery) !== -1
+      })
+
+    suggestions = searchResultBlocks.map(block => (
+      <PageLink
+        pageBlockId={block.id}
+        className="list-group-item list-group-item-action bg-dark text-light"
+        key={block.id}
+        onClick={clearSearch}
+      >
+        {block.text}
+      </PageLink>
+    ))
+  }
 
   return (
     <div className="navbar">
-      <i className="bi bi-search" />
-      <input
-        type="text"
-        className="form-control navbar__search"
-        placeholder="Find or Create Page"
-        onChange={(event) => dispatch(updateSearchQuery({ query: event.target.value }))}
-      />
-      {suggestions.length > 0 &&
-        <div className="navbar__search-results">
-          <ul className="list-group list-group-flush">
-            {suggestions}
-          </ul>
-        </div>
-      }
+      <div></div>
+      <div className="navbar__search">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Find or Create Page"
+          onChange={(event) => dispatch(updateSearchQuery({ query: event.target.value }))}
+        />
+        {suggestions.length > 0 &&
+          <div className="navbar__search-results">
+            <ul className="list-group list-group-flush">
+              {suggestions}
+            </ul>
+          </div>
+        }
+      </div>
     </div>
   )
 }
