1be6d1cd46dbd2b5c1abc001f98a7e355100f362
Connor Finley <connor.finley5@gmail.com>
2021-02-03 20:33:13 -0500

    Add save button for saving state as JSON

--- src/features/file-pane/FilePane.js
+++ src/features/file-pane/FilePane.js
@@ -2,15 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { StarFill } from "react-bootstrap-icons";
+import { saveAs } from 'file-saver'
 
 import './file-pane.scss'
 
 import PageLink from '../links/PageLink'
 
 const FilePane = props => {
-
+  const s = useSelector(state => state)
   const blocks = useSelector(state => state.blocks)
   const favoriteBlockIds = useSelector(state => state.filePane.favoriteBlockIds)
+
+  const save = () => {
+    console.log(s)
+    const jsonifiedState = JSON.stringify(s)
+    const file = new Blob([jsonifiedState], { type: 'text/json;charset=utf-8' })
+    saveAs(file, "free-roam.json");
+  }
 
   const pageListItems = favoriteBlockIds.map(blockId => {
     const block = blocks[blockId]
@@ -28,6 +36,10 @@ const FilePane = props => {
   return (
     <div className="file-pane">
       <Link to="/"><h2 className="text-light text-center">Free-roam</h2></Link>
+      <div className="d-flex justify-content-center">
+        <button className="btn btn-secondary mr-3">Load</button>
+        <button className="btn btn-secondary" onClick={save}>Save</button>
+      </div>
       <hr />
       <h5><StarFill color="white" width="15px" style={{ marginRight: "10px" }} />Shortcuts</h5>
       <div className="list-group list-group-flush mt-3">
