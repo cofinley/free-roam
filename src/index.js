1b1abfd46f845354a7824b93996b1b77045c920d
Connor Finley <connor.finley5@gmail.com>
2021-02-04 15:20:58 -0500

    Add alert on tab/window close in order to save work

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
+window.addEventListener('beforeunload', event => {
+  event.preventDefault()
+  event.returnValue = ''
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
